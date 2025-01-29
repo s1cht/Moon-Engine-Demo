e59cf306edee4956026e7716bddf7552cff3f3ff
s1cht <sachnikitus@gmail.com>
2025-01-29 21:24:56 +0200

    Fixed events

--- Engine/Source/Core/Events/Event.h
+++ Engine/Source/Core/Events/Event.h
@@ -37,7 +37,7 @@ enum EventCategory
 #define EVENT_CLASS_TYPE(type)\
     static EventType GetStaticType() { return EventType::##type; }\
     virtual EventType GetEventType() const override { return GetStaticType(); }\
-    virtual const char* GetName() const override { return #type; }
+    virtual const TCHAR* GetName() const override { return #type; }
 
 #define EVENT_CLASS_CATEGORY(category)\
     virtual EVENT_CATEGORY GetEventCategory() const override { return category; }
