00eb39408d812b89647ef6858217895dc64a75bb
s1cht <sachnikitus@gmail.com>
2025-03-24 12:42:34 +0200

    Pered ubiistvom

---
+++ EngineCore/Core/Misc/Pair.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Pawm
+{
+	template <class ValueType1, class ValueType2>
+	struct Pair
+	{
+	public:
+		using ValueDataType1 = ValueType1;
+		using ValueDataType2 = ValueType2;
+
+	public:
+		Pair() {};
+		Pair(ValueDataType1 value1, ValueDataType2 value2)
+		{
+			Value1 = value1;
+			Value2 = value2;
+		}
+		Pair(const Pair& other)
+		{
+			Value1 = other.Value1;
+			Value2 = other.Value2;
+		}
+		~Pair()
+		{
+			Value1.~ValueDataType1();
+			Value2.~ValueDataType2();
+		}
+
+
+	public:
+		bool operator==(const Pair& other)
+		{
+			return (this->Value1 == other.Value1 && this->Value2 == other.Value2);
+		}
+
+	public:
+		ValueDataType1 Value1;
+		ValueDataType2 Value2;
+
+	};
+}
