23392d7508fead6936bd6d81351c28984fdc7f71
s1cht <sachnikitus@gmail.com>
2025-03-28 12:12:09 +0200

    Update Map.h

---
+++ EngineCore/Core/Containers/Map.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Core.h"
+#include "Core/Utils/Logging/Logger.h"
+#include "Core/Memory/PawnMemory.h"
+#include "Core/Misc/Assertion.h"
+
+namespace Pawn
+{
+	template <class keyType, class valType, class _hasher /*= Pawn::Memory::Hasher*/, class _allocator = Memory::Allocator<void>>
+	class UnorderedMap
+	{
+
+	};
+
+}
